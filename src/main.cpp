bff0a5d592c2ea8ee25200ea3586d7bd351a11d3
Henrik Schack <hschack@gmail.com>
2026-01-05 15:05:08 +0100

    1

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,37 @@
 #define SEC_2_ON_LED PB7 // Digital output
 #define SEC_2_OFF_LED PB6 // Digital output
 
-int OnTime1Value = 100;
+int16_t OnTime1Value = 100;
 int16_t OffTime1Value = 0;
 int16_t OnTime2Value = 0;
 int16_t OffTime2Value = 0;
 //--------------------------------------------------------------
+// ------------------- Function prototypes -------------------
+void setup();
+void sampleAdc();
 
 
+
+void loop() {
+  sampleAdc();
+
+  delay(1000); 
+
+}
+
+// Sample all ADC channels
+void sampleAdc() {
+  OnTime1Value = analogRead(POT_ON_TIME_1);
+  OffTime1Value = analogRead(POT_OFF_TIME_1);
+  OnTime2Value = analogRead(POT_ON_TIME_2);
+  OffTime2Value = analogRead(POT_OFF_TIME_2);  
+  Serial1.println("Reading potentiometers...");
+  Serial1.print(OnTime1Value); Serial1.print(", ");
+  Serial1.print(OffTime1Value); Serial1.print(", "); 
+  Serial1.print(OnTime2Value); Serial1.print(", ");
+  Serial1.println(OffTime2Value);
+}
+// Setup function
 void setup() {
   Serial1.setRx(PA10);
   Serial1.setTx(PA9);
@@ -29,7 +53,7 @@ void setup() {
   delay(10);
   
 Serial.println("\nStarting up...");
-  analogReadResolution(12);
+  //analogReadResolution(12);
   pinMode(POT_ON_TIME_1, INPUT_ANALOG);
   pinMode(POT_OFF_TIME_1, INPUT_ANALOG);
   pinMode(POT_ON_TIME_2, INPUT_ANALOG);
@@ -45,19 +69,4 @@ Serial.println("\nStarting up...");
   digitalWrite(SEC_2_ON_LED, LOW);
   pinMode(SEC_2_OFF_LED, OUTPUT); 
   digitalWrite(SEC_2_OFF_LED, LOW); 
-}
-
-void loop() {
-OnTime1Value = analogRead(POT_ON_TIME_1);
-OffTime1Value = analogRead(POT_OFF_TIME_1);
-OnTime2Value = analogRead(POT_ON_TIME_2);
-OffTime2Value = analogRead(POT_OFF_TIME_2);  
-Serial1.println("Reading potentiometers...");
-Serial1.print(OnTime1Value); Serial1.print(", ");
-Serial1.print(OffTime1Value); Serial1.print(", "); 
-Serial1.print(OnTime2Value); Serial1.print(", ");
-Serial1.println(OffTime2Value);
-
-  delay(1000); 
-
 }
